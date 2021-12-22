8442e1c7009445136fe025dcdc30fca81a4788af
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-12-22 15:02:42 +0500

    added functionality

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import { Wrapper } from './style';
-import Sofa from '../../assets/images/sofa.png';
-import Chair from '../../assets/images/furniture1.png';
-import Chair1 from '../../assets/images/furniture2.png';
-import Chair2 from '../../assets/images/furniture3.png';
+import Data from './mock/data';
 
-export default class index extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <Wrapper.Container>
-          <Wrapper.Text>Top Collections 2022</Wrapper.Text>
-          <Wrapper.Info>We Serve Your Dream Furniture</Wrapper.Info>
-          <Wrapper.Discount>Get 50% off all products</Wrapper.Discount>
-          <Wrapper.Button>SHOP NOW</Wrapper.Button>
-        </Wrapper.Container>
-        <Wrapper.Sofa>
-          <img src={Sofa} alt='' width='350px' height='350px' />
-        </Wrapper.Sofa>
-        <Wrapper.Furniture>
-          <img src={Chair} alt='' />
-          <img src={Chair1} alt='' />
-          <img src={Chair2} alt='' />
-        </Wrapper.Furniture>
-      </Wrapper>
-    );
-  }
-}
+const Shaxboz = () => {
+  const [state, setState] = useState(0)
+
+  return (
+    <Wrapper>
+      <Wrapper.Container>
+        <Wrapper.Text>Top Collections 2022</Wrapper.Text>
+        <Wrapper.Info>We Serve Your Dream Furniture</Wrapper.Info>
+        <Wrapper.Discount>Get 50% off all products</Wrapper.Discount>
+        <Wrapper.Button>SHOP NOW</Wrapper.Button>
+      </Wrapper.Container>
+      <Wrapper.Sofa>
+        <img src={Data[state]} alt='' style={{width: '354', cursor:'pointer'}} />
+      </Wrapper.Sofa>
+      <Wrapper.Furniture onClick={()=> setState(1)} style={{cursor:'pointer' }}>
+        <img  src={Data[1]} alt='' style={{ backgroundColor: 'white' }}/>
+        <img src={Data[2]} alt='' style={{ backgroundColor: 'white' }} />
+        <img src={Data[3]} alt='' style={{ backgroundColor: 'white' }} />
+      </Wrapper.Furniture>
+    </Wrapper>
+  );
+};
+
+export default Shaxboz;
---
+++ src/Components/Banner/mock/data.js
@@ -0,0 +1,8 @@
+import Sofa from '../../../assets/images/sofa.png';
+import Chair from '../../../assets/images/furniture1.png';
+import Chair1 from '../../../assets/images/furniture2.png';
+import Chair2 from '../../../assets/images/furniture3.png';
+
+const Data = [Sofa, Chair, Chair1, Chair2];
+
+export default  Data;
